46d27eab8a1c0d905028a2da097369533b741170
won seob shin <wonseob07@gmail.com>
2019-05-29 19:42:40 +0000

    fixed char pos

--- character_positioning/character_positioning.js
+++ character_positioning/character_positioning.js
@@ -1,31 +1,48 @@
-function countLetters(str){
-  var obj = {};
-  var char =''
+// function countLetters(str){
+//   var obj = {};
+//   var char =''
+//   var arr = []
+//   for(i in str){
+//     if(str[i] !== ' '){
+//       obj[str[i]] = 'at '
 
-  for(i in str){
-    if(str[i] !== ' '){
-      obj[str[i]] = 'at '
-    }
-    if(str[i] !== ' '){
-      var foo = ''
-      if(obj[str[i]] === 'at ')
-        obj[str[i]] += i
-      else
-        obj[str[i]] += ', ' + i
+//     }
+//   }
+
+//   for(i in str){
+//     if(str[i] !== ' '){
+//       var foo = ''
+//       if(obj[str[i]] === 'at ')
+//         obj[str[i]] += i
+//       else
+//         obj[str[i]] += ', ' + i
+//       // if(i !== )
+//       //   obj[str[i]] += ','
+//     }
+//   }
+
+//   // for(i in str){
+//   //   obj[]
+//   // }
+//   return obj;
+// }
+
+function countLetters(str){
+  var charArr = str.split(' ').join("").split('');
+  var obj = {};
+
+  for(var i in charArr){
+    if(obj[charArr[i]]){
+      obj[charArr[i]].push(i);
+    } else {
+      obj[charArr[i]] = [i];
     }
   }
 
-  // for(i in str){
-  //     // if(i !== )
-  //     //   obj[str[i]] += ','
-  //   }
-  // }
-
-  // // for(i in str){
-  // //   obj[]
-  // // }
   return obj;
+
 }
 
 var it = countLetters("lighthouse in the house")
 console.log(it)
+
